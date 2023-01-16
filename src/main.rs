6d6aa27f4b9631f4eb2dd0b01b1e833a31b86a15
Washio Yusaku <washioyusaku@icloud.com>
2023-01-16 14:56:41 +0900

    add: ファイル読み込み関数実装 (#issue number)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+extern crate csv;
+
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+fn fopen(path: &OsString) -> Result<File, Box<dyn Error>> {
+    let file = File::open(path)?;
+    Ok(file)
+}
+
+fn csv_parse(file: File) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = match get_first_arg() {
+        Ok(file_path) => file_path,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            process::exit(1);
+        }
+    };
+
+    match fopen(&file_path) {
+        Ok(file) => println!("Successfully opened {:?}", file),
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            process::exit(1);
+        }
+    }
+
+    match csv_parse(fopen(&file_path).unwrap()) {
+        Ok(_) => println!("Successfully parsed CSV"),
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            process::exit(1);
+        }
+    }
 }
